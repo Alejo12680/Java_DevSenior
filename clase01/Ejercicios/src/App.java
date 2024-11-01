1dd91c79a3e8d0c2f0ceb0194e8ce74ef1c32c5e
Alejo12680 <alejo152725@gmail.com>
2024-11-01 02:02:24 -0500

    init proyect

---
+++ clase01/Ejercicios/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    // Atajo de Visual para el main es "psvm" o "main"
+    public static void main(String[] args) throws Exception {
+        holaMundo();
+    }
+
+    /* Metodos (Ejercicios de la Clase 01) */
+    public static void holaMundo() {
+        System.out.println("Hello, World!");
+    }
+    
+}
